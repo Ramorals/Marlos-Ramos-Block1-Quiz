8d44abb9fcc639eb606ecb7a62726d6ed66cd2ec
Marlos Ramos <arsa.ramos.up@phinmaed.com>
2023-12-09 09:44:06 +0800

    1st commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Marlos-Ramos-block1-Quiz"
+include(":app")
